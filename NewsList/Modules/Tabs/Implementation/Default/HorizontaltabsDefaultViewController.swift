80d6fc835437ba60ea5b5d0664a7eacd5593a34e
Rathish Kannan <rathish.k@sunandsandsports.com>
2018-12-18 13:54:09 +0400

    View Layer - UIKit file protocol

---
+++ NewsList/Modules/Tabs/Implementation/Default/HorizontaltabsDefaultViewController.swift
@@ -0,0 +1,16 @@
+//
+//  HorizontaltabsDefaultViewController.swift
+//  HorizontaltabsDefault
+//
+//  Created by Rathish Kannan on 2018. 12. 18..
+//  Copyright © 2018. Rathish Kannan. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class HorizontaltabsDefaultViewController: UIViewController, HorizontaltabsViewController {
+
+    var presenter: HorizontaltabsPresenter?
+
+}
