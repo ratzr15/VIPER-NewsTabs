60f98741de2bce044e458c6d383ea975fcb06a64
Rathish Kannan <rathish.k@sunandsandsports.com>
2018-12-18 12:44:48 +0400

    ROUTER Layer - The navigation logic of the application

---
+++ NewsList/Modules/Tabs/Implementation/Default/HorizontaltabsDefaultRouter.swift
@@ -0,0 +1,17 @@
+//
+//  HorizontaltabsDefaultRouter.swift
+//  HorizontaltabsDefault
+//
+//  Created by Rathish Kannan on 2018. 12. 18..
+//  Copyright © 2018. Rathish Kannan. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class HorizontaltabsDefaultRouter: HorizontaltabsRouter {
+
+    weak var presenter: HorizontaltabsPresenter?
+    weak var viewController: UIViewController?
+
+}
